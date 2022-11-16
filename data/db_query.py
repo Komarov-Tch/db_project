1b8e29b8209406dddefd88385a2d5e085a6728e5
KomarovEA <Komarovegr@gmail.com>
2022-11-16 16:51:01 +0700

    add function add_country

--- data/db_query.py
+++ data/db_query.py
@@ -4,12 +4,32 @@ from sqlalchemy.orm import sessionmaker
 from models import *
 
 
-def add_genre(session, list_genre):
-    for genre in list_genre:
-        add_ = Genre(title=genre)
-        session.add(add_)
-    session.commit()
+def add_genre(session, list_genre: tuple) -> bool:
+    """
+    тут информация о функции add_genre
+    """
+    try:
+        for genre in list_genre:
+            add_ = Genre(title=genre)
+            session.add(add_)
+        session.commit()
+        return True
+    except:
+        return False
 
+
+def add_country(session, list_country: tuple) -> bool:
+    """
+    информация о функции
+    """
+    try:
+        for country in list_country:
+            add_ = Country(title=country)
+            session.add(add_)
+        session.commit()
+        return True
+    except:
+        return False
 
 
 base_name = 'тут название вашей базы данных'
@@ -19,10 +39,10 @@ create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
-
+list_country = ('Россия', 'США', 'Япония', 'Англия', 'Германия')
 list_genre = ('ужасы', 'триллер', 'комедия',
               'драма', 'фантастика', 'фентези',
               'документальные', 'мемуары', 'детектив', 'боевик')
 add_genre(session, list_genre)
-
+add_country(session, list_country)
 session.close()
